f59c79c0f0dbd178f3ce50ce5d37fea295d29d85
Yuki23501 <82694568+Yuki23501@users.noreply.github.com>
2022-03-08 21:01:28 +0900

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -1,17 +1,11 @@
-const app = new Vue({
-  el:"#app",
-  data: {
-    count: 3*24*60*60,
-    color: "black"
-  },
-  created() {
-    setInterval(()=>{
-      this.count = this.count - 1
-      if(this.color === "black") {
-        this.color= "red"
-      } else {
-        this.color= "black"
-      }
-    },1000)
-  }
-})
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
+let c = 6 * 60 * 60;
+setInterval(() => {
+  $("#hour").text(Math.floor(c / 3600));
+  $("#min").text(Math.floor((c % 3600) / 60));
+  $("#sec").text(Math.floor(c % 60));
+  c = c - 1;
+}, 1000);
+$("#device").text(platform.description);
+$("#date").text(new Date().toLocaleString("ja-JP"));
